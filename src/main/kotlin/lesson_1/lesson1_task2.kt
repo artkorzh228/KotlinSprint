67ba550cce3b309e4b39859e40314211c9d46122
artkorzh228 <aleksandr.k.197762@gmail.com>
2024-10-10 19:42:56 +0200

    KS-1-2 problem solving

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,6 +1,12 @@
 package org.example.lesson_1
 
 fun main(){
-    println("xxxx")
+    val amountOfOrders: Int = 75
+    val textForThanking: String = "Thank you for buying $amountOfOrders items in our shop."
+    println(textForThanking)
 
+    var amountOfEmployees: Int = 2000
+    // println(amountOfEmployees)
+    amountOfEmployees--
+    println("The amount of employees is: $amountOfEmployees")
 }
