28b7dcdedeef8d449281d84e47d7d03f68fef257
artkorzh228 <aleksandr.k.197762@gmail.com>
2024-10-11 20:59:32 +0200

    KS-1-5 Hours, minutes, seconds double digit problem solving

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,21 @@
 package org.example.lesson_1
 
 fun main() {
-    // вводим полное кол-во секунд
+
     val totalSeconds = 5409
-    val hours = totalSeconds / 3600
-    // тут считаем остаток от деления если кол-во секунд не кратно 3600
+    val secondsInMinute = 60
+    val secondsInHour = secondsInMinute * secondsInMinute
+    val hours = totalSeconds / secondsInHour
+    val remainingSecondsInHour = totalSeconds % secondsInHour
+    val minutes = remainingSecondsInHour / secondsInMinute
+    val seconds = totalSeconds % minutes
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
+}
+  /*  // тут считаем остаток от деления если кол-во секунд не кратно 3600
     val remainingSecondsAfterHours = totalSeconds % 3600
-    val minutes = remainingSecondsAfterHours / 60
+    /*val minutes = remainingSecondsAfterHours / 60
     // считаем остаток от деления если кол-во оставшихся секунд не кратно 60
     val seconds = remainingSecondsAfterHours % 60
 
-    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
-}
+
+*/
