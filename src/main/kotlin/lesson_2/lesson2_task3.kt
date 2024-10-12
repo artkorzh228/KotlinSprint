59bffbc53020bed5840b941e5627bf12ede3a6c4
artkorzh228 <aleksandr.k.197762@gmail.com>
2024-10-13 00:44:56 +0200

    KS-2-3 time adding problem solving

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,21 +1,17 @@
 package org.example.lesson_2
 
 fun main() {
-    var startHour: Int = 9
-    var startMinute: Int = 37
-
+    val startHour: Int = 9
+    val startMinute: Int = 37
     val hour: Int = 60
-    var time: Int = 457
-    while (time > hour) {
-        time -= hour
-        startHour++
-    }
-    if (time < hour)
-    {
-        time += startMinute
-        startHour++
-        startMinute = time - hour
-    }
+    val time: Int = 457
+    val departureTime: Int = startHour * hour + startMinute
+    val arrivalTime: Int = departureTime + time
+    val arrivalHour: Int = arrivalTime / hour
+    val arrivalMinute: Int = arrivalTime % hour
+    println("The train will arrive at: $arrivalHour:$arrivalMinute")
 
-    println("$startHour:$startMinute")
+
+
 }
+
