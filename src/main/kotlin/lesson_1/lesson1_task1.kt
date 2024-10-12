43fe5aac55d4228b7859ba0b462a7db95f7df80c
artkorzh228 <aleksandr.k.197762@gmail.com>
2024-10-13 01:24:36 +0200

    KS-2-4 buff percentage problem solving

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,8 +1,6 @@
 package org.example.lesson_1
 
 fun main() {
-    val amountOfOrders: Int = 75
-    val textForThanking: String = "Thank you for buying $amountOfOrders items in our shop."
-    println(textForThanking)
-
+    println(" ")
+    println(".....")
 }
